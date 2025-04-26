1c6014763f262e56ddc544341043232c08bf29a8
MOODPA <arquitetocaiquetavares@gmail.com>
2025-04-25 21:33:24 -0300

    Update sobre.js

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import header from '../components/header';
-import footer from '../components/hooter';
+import footer from '../components/footer';
 
 export default function Sobre() {
   return (
